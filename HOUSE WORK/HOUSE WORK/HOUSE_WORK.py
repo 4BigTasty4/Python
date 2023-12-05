da93b911e770e2be8b4d78e69cbb81c7745044e7
4BigTasty4 <eldanizyolchuyev@gmail.com>
2023-12-05 16:17:23 +0400

    Python
    Python

---
+++ HOUSE WORK/HOUSE WORK/HOUSE_WORK.py
@@ -0,0 +1,240 @@
+#print("Hello User")
+#flash = int(input("Please Tell Me Your Height: "))
+#flash1 = input("Select A Height Unit: ")
+#flash2 = int(flash / 100)
+#flash3 = int(flash % 100)
+#print(f"You Are {flash2}M {flash3}SM ")
+
+
+
+#print("Hello User")
+#growth = int(input("Tell Me Your Growth in centimeters: "))
+#growth1 = int(growth / 100)
+#growth2 = int(growth % 100)
+#print(f"You Are {growth1}M {growth2}SM ")
+
+
+
+#print("Hello User")
+#money = float(input("Enter The Amount Of Money: "))
+#money1 = float(input("Select The Exchange Rate: "))
+#money2 = money * money1
+#print(f"Your Amount At The Rate: {money2} ")
+
+
+
+#print("Hello User")
+#test = input("Enter A Word: ")
+#test1 = input("Choose A Number Up To 10: ")
+#test2 = str(test) * int(test1)
+#print(test2)
+
+
+
+
+
+
+
+
+
+
+import abc
+
+
+#print("Hello User")
+#print("Question: 1\nHow Much Will 2 + 2: ")
+#print("A)5\nB)4\nC)2\nD)8")
+#pride = input("Input Your Answer: ")
+#answer = "A"
+#answer1 = "B"
+#answer2 = "C"
+#answer3 = "D"
+#if pride == answer1:
+#    print("Succses\nYou Win 500$")
+#elif pride == answer:
+#    print("You Lose")
+#elif pride == answer2:
+#    print("You Lose")
+#elif pride == answer3:
+#    print("You Lose")
+#if pride == answer1:
+#    print("Question: 2\nHow Much Will 5 * 4: ")
+#    print("A)35\nB)13\nC)20\nD)27")
+#    pride = input("Input Your Answer: ")
+#    act = "A"
+#    act1 = "B"
+#    act2 = "C"
+#    act3 = "D"
+#    if pride == act2:
+#     print("Succses\nYou Win 1000$")
+#    elif pride == act:
+#     print("You Lose")
+#    elif pride == act1:
+#     print("You Lose")
+#    elif pride == act3:
+#     print("You Lose")
+#    if pride == act2:
+#     print("Question: 3\nHow Much Will 145 - 150: ")
+#     print("A)-5\nB)125\nC)-55\nD)295")
+#     pride = input("Input Your Answer: ")
+#     akp = "A"
+#     akp1 = "B"
+#     akp2 = "C"
+#     akp3 = "D"
+#     if pride == akp:
+#       print("Succses\nYou Win 1500$")
+#     elif pride == akp1:
+#       print("You Lose")
+#     elif pride == akp2:
+#       print("You Lose")
+#     elif pride == akp3:
+#       print("You Lose")
+#     if pride == akp:
+#       print("Question: 4\nHow Much Will 50 / 25: ")
+#       print("A)5\nB)2\nC)1\nD)0")
+#       pride = input("Input Your Answer: ")
+#       ark = "A"
+#       ark1 = "B"
+#       ark2 = "C"
+#       ark3 = "D"
+#       if pride == ark1:
+#        print("Succses\nYou Win 2000$")
+#       elif pride == ark:
+#        print("You Lose")
+#       elif pride == ark2:
+#        print("You Lose")
+#       elif pride == ark3:
+#        print("You Lose")
+#       if pride == ark1:
+#         print("Question: 5\nHow Much Will (10 + 5)*5: ")
+#         print("A)5\nB)75\nC)25\nD)15")
+#         pride = input("Input Your Answer: ")
+#         apk = "A"
+#         apk1 = "B"
+#         apk2 = "C"
+#         apk3 = "D"
+#         if pride == apk1:
+#           print("Succses\nYou Win 2500$")
+#         elif pride == apk1:
+#          print("You Lose")
+#         elif pride == apk1:
+#          print("You Lose")
+#         elif pride == apk3:
+#          print("You Lose")
+#         if pride == apk1:
+#          print("Question: 6\nHow Much Will (55 + 5)/2: ")
+#          print("A)10\nB)30\nC)15\nD)28")
+#          pride = input("Input Your Answer: ")
+#          atk = "A"
+#          atk1 = "B"
+#          atk2 = "C"
+#          atk3 = "D"
+#         if pride == atk1:
+#           print("Succses\nYou Win 3000$")
+#         elif pride == atk:
+#          print("You Lose")
+#         elif pride == atk2:
+#          print("You Lose")
+#         elif pride == atk3:
+#          print("You Lose")
+#         if pride == atk1:
+#           print("Question: 7\nHow Much Will 5 ** 2: ")
+#           print("A)55\nB)25\nC)10\nD)35")
+#           pride = input("Input Your Answer: ")
+#           afk = "A"
+#           afk1 = "B"
+#           afk2 = "C"
+#           afk3 = "D"
+#           if pride == afk1:
+#            print("Succses\nYou Win 3500$")
+#           elif pride == afk:
+#            print("You Lose")
+#           elif pride == afk2:
+#            print("You Lose")
+#           elif pride == afk3:
+#            print("You Lose")
+#           if pride == afk1:
+#             print("Question: 8\nHow Much Will (55-25)*3: ")
+#             print("A)60\nB)90\nC)45\nD)55")
+#             pride = input("Input Your Answer: ")
+#             ask = "A"
+#             ask1 = "B"
+#             ask2 = "C"
+#             ask3 = "D"
+#             if pride == ask1:
+#               print("Succses\nYou Win 4000$")
+#             elif pride == ask:
+#                print("You Lose")
+#             elif pride == ask2:
+#                print("You Lose")
+#             elif pride == ask3:
+#                print("You Lose")
+#             if pride == ask1:
+#               print("Question: 9\nHow Much Will 3*8-4/2: ")
+#               print("A)12\nB)8\nC)24\nD)22")
+#               pride = input("Input Your Answer: ")
+#               ak = "A"
+#               ak1 = "B"
+#               ak2 = "C"
+#               ak3 = "D"
+#               if pride == ak3:
+#                 print("Succses\nYou Win 4500$")
+#               elif pride == ak:
+#                 print("You Lose")
+#               elif pride == ak1:
+#                 print("You Lose")
+#               elif pride == ak2:
+#                 print("You Lose")
+#               if pride == ak3:
+#                  print("Question: 10\nHow Much Will 100/5*4: ")
+#                  print("A)80\nB)140\nC)50\nD)200")
+#                  pride = input("Input Your Answer: ")
+#                  akr = "A"
+#                  akr1 = "B"
+#                  akr2 = "C"
+#                  akr3 = "D"
+#                  if pride == akr:
+#                     print("Succses\nYou Win 5000$\nCongratulations You Won A Prize Of: 2750")
+#                  elif pride == akr1:
+#                     print("You Lose")
+#                  elif pride == akr2:
+#                     print("You Lose")
+#                  elif pride == akr3:
+#                     print("You Lose")
+
+       
+
+
+
+
+#print("Hello User")
+#number = int(input("Input Your Number: "))
+#degree = int(input("Input Degree: "))
+#summa = 1
+#while True:
+#    degree > 0
+#    summa *= number
+#    degree -= 1
+#    if degree == 0:
+#        print(summa)
+#        break
+#    elif degree < 0:
+#        print("Error")
+#        break
+
+
+
+#number = int(input())
+#step = 1
+#while True:
+#    if number >= 1:
+#        step *= number
+#        number -= 1 
+#    elif step > 0:
+#        print(step)
+#        break
+    
+
+word = ["one", "two", 3]
+abbas = word.reverse("one")
+print(abbas)
