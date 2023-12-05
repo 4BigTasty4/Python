ce55d9de20500273a33d8a17b84083bf3ea87ba9
4BigTasty4 <eldanizyolchuyev@gmail.com>
2023-12-05 16:20:08 +0400

    Python
    Python

---
+++ REpit/REpit/REpit.py
@@ -0,0 +1,733 @@
+#print("Hello User")
+#name = input("Input Ypor Name: ")
+#surname = input("Input Your Surname: ")
+#print(f"You {name} {surname}")
+
+
+
+#print("Hello User")
+#number = int(input("Input Your Number: "))
+#degree = int(input("Input Your Degree: "))
+#summa = number ** degree
+#print(summa)
+
+
+#N1
+#print("Hello User")
+#height = int(input("Enter your height In SM: "))
+#summa = height // 100
+#power = height % 100
+#print(f"You Are {summa} M And {power} Sm")
+
+#N2
+#print("Hello User")
+#money = float(input("Input Your Money: "))
+#kurs = float(input("Choose Your Kurs: "))
+#summa = money * kurs
+#print(f"Your Money In Kurs: {summa} ")
+
+
+
+#print("Hello User")
+#firstWord = str(input("Input The Word: "))
+#secondWord = str(input("Input The Word2: "))
+#summa = firstWord == secondWord
+#print(summa)
+
+
+
+
+#print("Hello User")
+#number = float(input("Input Your Number: "))
+#condition = number <= 10
+#print(condition)
+
+
+
+
+
+
+#print("Hello User")
+#visa = True
+#money = float(input("Input Your Money: "))
+#summa = money >= 41000 and visa == True
+#print(summa)
+
+
+
+
+#print("Hello User")
+#word = str(input("Input the Word: "))
+#number = int(input("Input Your Number: "))
+#if number <= 10:
+#   summa = number * word
+#   print(summa)
+#if number > 10:
+#    print("Error")
+
+
+
+
+#print("Hello User")
+#var = 777
+#if var == int:
+#    print("It Is Integer")
+#if var != int:
+#    print("It Isnt Integer")
+
+
+
+
+#print("Hello User")
+#money = float(input("Input Your Money: "))
+#visa = input("Do You Have Visa?: ")
+#if money >= 40000 and visa == "yes":
+#    print("Succses")
+#if money < 40000 and visa == "no":
+#    print("Error")
+#if money < 40000 and visa == "yes":
+#    print("Error")
+#if money >= 40000 and visa == "no":
+#    print("Error")
+
+
+
+
+
+#print("Hello user")
+#money = float(input("Input Your Money: "))
+#kurs = input("Choose Your Kurs: ")
+#euro = "1.85"
+#usd = 1.7
+#tl = 0.12
+#if kurs != euro:
+#    print("Error")
+#if kurs != usd:
+#    print("Error")
+#if kurs != tl:
+#    print("Error")
+#if kurs == euro:
+#    summa = money * euro
+#    print(f"Your Money At The Rate: {summa}")
+#if kurs == usd:
+#    summa = money * usd
+#    print(f"Your Money At The Rate: {summa}")
+#if kurs == tl:
+#    summa = money * tl
+#    print(f"Your Money At The Rate: {summa}")
+
+
+
+
+#print("Hello User")
+#symbol = input("Input Your Symbol: ")
+#for i in symbol:
+
+#    print(i)
+
+
+#sss = "123"
+#ss = int(sss)
+#print(type(ss))
+#print(ss)
+
+
+
+
+#print("Hello User")
+#visa = True
+#money = float(input("Input Your Money: "))
+#if money >= 40000 and visa == True:
+#    print("Go To USA")
+#else:
+#    print("Go Home")
+
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#for i in range(0,number):
+#    i = "*"
+#    i *= number 
+
+
+    
+#    print(i)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#polzovatel vvodit 2 stroki
+#1 stroka pisetsya tex
+#2 stroka cto budet iskatsya vo 1 stroke
+#elis necego ne naydeno 
+#vvvodit search not found
+#esli nasli vvodit skolko raz v texte bil nayden search
+
+
+#print("Hello User")
+#text = input("Input Text: ")
+#searchText = input("Input The Search Word In Text: ")
+#search = searchText.count(str(text))
+#search1 = searchText.__contains__(text)
+#if searchText.__contains__(text):
+#    print(search1)
+#else:
+#    print(f"{search} is not found")
+
+
+#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+
+#print("Hello User")
+#number = int(input("input number: "))
+#for f in range (0,number):
+#    f = "*"
+#    f *= number
+#    number -= 1
+#    print(f)
+
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#for e in range (0,number):
+#    print("*"*(e+1))
+
+
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#summa = number - number
+#power = summa + 1
+#for e in range (0,number):
+#    e = "*"
+#    e *= power
+#    power += 1
+#    print(e)
+
+
+
+
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#for R in range(0,number):
+#    print(" "*(number-1), "*"*(R+1))
+#    number -= 1
+
+
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#for p in range(0,number):
+#    print(" " * p,"*" * number)
+#    number -= 1 
+
+
+    
+
+
+#print("Hello User")
+#number = int(input("Input Number: "))
+#for n in range (0,number + 1):
+#    print("*"*(n+1))
+
+#for n in range(0,number + 1):
+#    print("*" * (number + 1))
+#    number -= 1
+
+
+
+
+#print("Hello User")
+#symbol = str(input("Input Symbol: "))
+#number = int(input("Input Your Range: "))
+#def Mysymbol():
+#    global number, symbol
+#    for u in range(0,number + number):
+#       if u % 2 != 0:
+#           print(" " * number, symbol * u)
+#           number -= 1
+
+
+#Mysymbol()
+
+
+
+
+#print("Hello User")
+#money = float(input("Input Money: "))
+#vallue = str(input("Input Kurs: "))
+
+#usd = 1.7
+#euro = 1.79
+#tl = 0.11
+
+#def Myvalue():
+#    global valluye, usd, euro, tl
+#    if vallue == "usd" or vallue == "USD" or vallue == "Usd":
+#        print(money * usd)
+#    elif vallue == "euro" or vallue == "Euro" or vallue == "EURO":
+#        print(money * euro)
+#    elif vallue == "tl" or vallue == "Tl" or vallue == "TL":
+#        print(money * tl)
+#    else:
+#        print("Error")
+
+#Myvalue()
+
+
+
+
+
+#print("Hello User")
+#wifiName = "qwerty"
+#wifiPasword = "qwerty123"        
+
+#def Wifi():
+#    global wifiName, wifiPasword
+#    while True:
+#        wifi = input("Input Wifi Name: ")
+#        pasword = input("Input Wifi Pasword: ")
+#        if wifi == wifiName and pasword == wifiPasword:
+#            print("Connect....")
+#            break
+#        else:
+#            print("Error")
+#            continue
+
+
+#Wifi()
+
+
+
+
+
+#print("Hello User")
+#login = "Angry"
+#pasword = "Angry333"
+#def Login():
+#    while True:
+#        userLogin = input("Input Login: ")
+#        userPasword = input("Input Pasword: ")
+#        if login == userLogin and pasword == userPasword:
+#            print("Succses")
+#            break
+#        else:
+#            print("Error")
+#            Login()
+
+#Login()
+
+
+
+
+#def MyFunc(num):
+#    num+=10
+    
+#    return num
+#number = 100
+#MyFunc(number)
+
+
+
+#print("Hello user")
+#while True:
+#    text = input("Input text: ")
+#    writ = input("Input Separ: ")
+#    word = input("Input Word in Text: ")
+#    num = text.count(word)
+#    separ = text.split()
+#    if writ == " ":
+#        print(separ)
+#        print(num)
+#        break
+#    elif writ:
+#        print(text.split(writ))
+#        print(num)
+#        break
+#    else:
+#        print("\nError\n")
+#        continue
+
+
+
+#1
+#print("Hello User")
+#def MyPram(number, num):
+#    for f in range(number, num + 1):
+#        if f % 2 != 0:
+#            print(f)
+#number = int(input("Input Number: "))
+#num = int(input("Input Number 2: "))
+
+#MyPram(number, num)     
+
+
+#2
+#def MyDiapazon(number, number1, number2, number3):
+#    if number > number1 and number > number2 and number > number3:
+#        print(number)
+#    elif number1 > number and number1 > number2 and number1 > number3:
+#        print(number1)
+#    elif number2 > number and number2 > number1 and number2 > number3:
+#        print(number2)
+#    elif number3 > number and number3 > number1 and number3 > number2:
+#        print(number3)
+
+#number = int(input("Input Number: "))
+#number1 = int(input("Input Number1: "))
+#number2 = int(input("Input Number2: "))
+#number3 = int(input("Input Number3: "))
+
+#MyDiapazon(number, number1, number2, number3)
+
+
+
+
+
+
+#3
+#print("Hello User")
+#def MyGorizontVertical(length, direction, symbol):
+#    for t in range(1, leght):
+#        if direction == 1:
+#            num = (t + leght) - leght 
+#            num = t * symbol
+#            print(num) 
+
+#print("Hello User")
+#def MyGorizontVertical(leght, direction, symbol):
+#    for u in range(1,leght+1):
+#        if direction == 1:
+#            print(symbol)
+#        elif direction == -1:
+#            print(symbol * leght)
+#            leght -= leght + 1
+
+
+
+#leght = int(input("input leght: "))
+#direction = int(input("input direction 1 or -1: "))
+#symbol = input("input synbol: ")
+#MyGorizontVertical(leght, direction, symbol)
+
+
+#4
+#print("Hello User")
+#def MyDiapazon(number, num):
+#    for r in range(number, num):
+#        number += (r + 1) 
+#    print(number) 
+
+        
+#number = int(input("Iput Number: "))
+#num = int(input("Iput Number: "))
+#MyDiapazon(number, num)
+
+
+
+
+
+
+#print("Hello User")
+#print("Question: 1\nHow Much Will 2 + 2: ")
+#print("A)5\nB)4\nC)2\nD)8")
+#pride = input("Input Your Answer: ")
+#answer = "A"
+#answer1 = "B"
+#answer2 = "C"
+#answer3 = "D"
+#if pride == answer1:
+#    print("Succses\nYou Win 500$")
+#elif pride == answer:
+#    print("You Lose")
+#elif pride == answer2:
+#    print("You Lose")
+#elif pride == answer3:
+#    print("You Lose")
+#if pride == answer1:
+#    print("Question: 2\nHow Much Will 5 * 4: ")
+#    print("A)35\nB)13\nC)20\nD)27")
+#    pride = input("Input Your Answer: ")
+#    act = "A"
+#    act1 = "B"
+#    act2 = "C"
+#    act3 = "D"
+#    if pride == act2:
+#     print("Succses\nYou Win 1000$")
+#    elif pride == act:
+#     print("You Lose")
+#    elif pride == act1:
+#     print("You Lose")
+#    elif pride == act3:
+#     print("You Lose")
+#    if pride == act2:
+#     print("Question: 3\nHow Much Will 145 - 150: ")
+#     print("A)-5\nB)125\nC)-55\nD)295")
+#     pride = input("Input Your Answer: ")
+#     akp = "A"
+#     akp1 = "B"
+#     akp2 = "C"
+#     akp3 = "D"
+#     if pride == akp:
+#       print("Succses\nYou Win 1500$")
+#     elif pride == akp1:
+#       print("You Lose")
+#     elif pride == akp2:
+#       print("You Lose")
+#     elif pride == akp3:
+#       print("You Lose")
+#     if pride == akp:
+#       print("Question: 4\nHow Much Will 50 / 25: ")
+#       print("A)5\nB)2\nC)1\nD)0")
+#       pride = input("Input Your Answer: ")
+#       ark = "A"
+#       ark1 = "B"
+#       ark2 = "C"
+#       ark3 = "D"
+#       if pride == ark1:
+#        print("Succses\nYou Win 2000$")
+#       elif pride == ark:
+#        print("You Lose")
+#       elif pride == ark2:
+#        print("You Lose")
+#       elif pride == ark3:
+#        print("You Lose")
+#       if pride == ark1:
+#         print("Question: 5\nHow Much Will (10 + 5)*5: ")
+#         print("A)5\nB)75\nC)25\nD)15")
+#         pride = input("Input Your Answer: ")
+#         apk = "A"
+#         apk1 = "B"
+#         apk2 = "C"
+#         apk3 = "D"
+#         if pride == apk1:
+#           print("Succses\nYou Win 2500$")
+#         elif pride == apk1:
+#          print("You Lose")
+#         elif pride == apk1:
+#          print("You Lose")
+#         elif pride == apk3:
+#          print("You Lose")
+#         if pride == apk1:
+#          print("Question: 6\nHow Much Will (55 + 5)/2: ")
+#          print("A)10\nB)30\nC)15\nD)28")
+#          pride = input("Input Your Answer: ")
+#          atk = "A"
+#          atk1 = "B"
+#          atk2 = "C"
+#          atk3 = "D"
+#         if pride == atk1:
+#           print("Succses\nYou Win 3000$")
+#         elif pride == atk:
+#          print("You Lose")
+#         elif pride == atk2:
+#          print("You Lose")
+#         elif pride == atk3:
+#          print("You Lose")
+#         if pride == atk1:
+#           print("Question: 7\nHow Much Will 5 ** 2: ")
+#           print("A)55\nB)25\nC)10\nD)35")
+#           pride = input("Input Your Answer: ")
+#           afk = "A"
+#           afk1 = "B"
+#           afk2 = "C"
+#           afk3 = "D"
+#           if pride == afk1:
+#            print("Succses\nYou Win 3500$")
+#           elif pride == afk:
+#            print("You Lose")
+#           elif pride == afk2:
+#            print("You Lose")
+#           elif pride == afk3:
+#            print("You Lose")
+#           if pride == afk1:
+#             print("Question: 8\nHow Much Will (55-25)*3: ")
+#             print("A)60\nB)90\nC)45\nD)55")
+#             pride = input("Input Your Answer: ")
+#             ask = "A"
+#             ask1 = "B"
+#             ask2 = "C"
+#             ask3 = "D"
+#             if pride == ask1:
+#               print("Succses\nYou Win 4000$")
+#             elif pride == ask:
+#                print("You Lose")
+#             elif pride == ask2:
+#                print("You Lose")
+#             elif pride == ask3:
+#                print("You Lose")
+#             if pride == ask1:
+#               print("Question: 9\nHow Much Will 3*8-4/2: ")
+#               print("A)12\nB)8\nC)24\nD)22")
+#               pride = input("Input Your Answer: ")
+#               ak = "A"
+#               ak1 = "B"
+#               ak2 = "C"
+#               ak3 = "D"
+#               if pride == ak3:
+#                 print("Succses\nYou Win 4500$")
+#               elif pride == ak:
+#                 print("You Lose")
+#               elif pride == ak1:
+#                 print("You Lose")
+#               elif pride == ak2:
+#                 print("You Lose")
+#               if pride == ak3:
+#                  print("Question: 10\nHow Much Will 100/5*4: ")
+#                  print("A)80\nB)140\nC)50\nD)200")
+#                  pride = input("Input Your Answer: ")
+#                  akr = "A"
+#                  akr1 = "B"
+#                  akr2 = "C"
+#                  akr3 = "D"
+#                  if pride == akr:
+#                     print("Succses\nYou Win 5000$\nCongratulations You Won A Prize Of: 2750")
+#                  elif pride == akr1:
+#                     print("You Lose")
+#                  elif pride == akr2:
+#                     print("You Lose")
+#                  elif pride == akr3:
+#                     print("You Lose")
+
+
+
+
+
+
+#winers = ["Elnur, Zabil, Elvin"]
+#lose = ["Dan, Steve"]
+
+
+#print("Hello User")
+#global name, surname, bank, answer
+#def LoginGame(name, surname):
+ 
+
+# print(f"\n{name} {surname} Welcome to Millionaire Game! \n")
+
+
+# print("Victory table")
+# print("5-rd. question - 5000$")
+# print("4-rd. question - 4000$")
+# print("3-rd. question - 3000$")
+# print("2-nd. question - 2000$")
+# print("1-st. question - 1000$ \n")
+
+#name = input("Input Your Name: ")
+#surname = input("Input Your Surname: ")
+
+
+#LoginGame(name, surname)
+
+
+#bank = 0       
+#print("\nQuestion: 1\nHow Much Will 2 + 2: ")
+#print("A)5\nB)4\nC)2\nD)8")
+#answer = input("Input Your Answer: ")  
+#if answer == "B" or answer == "b":
+#    print("Succses\n")
+#    bank += 1000
+#else:
+#    print(f"\nYou Lose and Your Bank: {bank}$")
+#    lose.append(name)
+#    print(f"\nYou Are On The Losing List: {lose}")
+#if answer == "B" or answer == "b":
+#    print("Question: 2\nHow Much Will 5 * 4: ")
+#    print("A)35\nB)13\nC)20\nD)27")
+#    answer = input("Input Your Answer: ")
+#    if answer == "C" or answer == "c":
+#     print("Succses\n")
+#     bank += 2000
+#    else:
+#        print(f"You Lose and Your Bank: {bank}$")
+#        lose.append(name)
+#        print(f"\nYou Are On The Losing List: {lose}")
+#    if answer == "C" or answer == "c":
+#     print("Question: 3\nHow Much Will 145 - 150: ")
+#     print("A)-5\nB)125\nC)-55\nD)295")
+#     answer = input("Input Your Answer: ")
+#     if answer == "A" or answer == "a":
+#       print("Succses\n")
+#       bank += 3000
+#     else:
+#         print(f"You Lose and Your Bank: {bank}$")
+#         lose.append(name)
+#         print(f"\nYou Are On The Losing List: {lose}")
+#     if answer == "A" or answer == "a":
+#       print("Question: 4\nHow Much Will 50 / 25: ")
+#       print("A)5\nB)2\nC)1\nD)0")
+#       answer = input("Input Your Answer: ")
+#       if answer == "B" or answer == "b":
+#        print("Succses\n")
+#        bank += 4000
+#       else:
+#         print(f"You Lose and Your Bank {bank}$")
+#         lose.append(name)
+#         print(f"/nYou Are On The Losing List: {lose}")
+#       if answer == "B" or answer == "b":
+#         print("Question: 5\nHow Much Will (10 + 5)*5: ")
+#         print("A)75\nB)5\nC)25\nD)15")
+#         answer = input("Input Your Answer: ") 
+#         if answer == "A" or answer == "a":
+#           print("Succses\n")
+#           print(f"Congratulations You Won And Your Winnings Are: {bank}$ ")
+#           winers.append(name)
+#           print(f"\nYou Are On The List Of Winners: {winers}")
+#           bank += 5000
+#         else:
+#          print(f"You Lose and Your Bank: {bank}$")
+#          lose.append(name)
+#          print(f"\nYou Are On The Losing List: {lose}")
+
+
+
+
+
+#winers = ["Winers: Elnur, Zabil, Elvin"]
+#lose = ["Lose: Dan, Steve"]
+ 
+
+#name = input()
+#num = list(name)
+#winers.append(name)
+#print(winers)
+
+
+#def MyFunc(name):
+#    name = 100
+#    name += 1
+#MyFunc()
+
+
+
+#elif True:
+#    print("1")
+#if True:
+#    print("2")
+#if False:
+#    print("3")
+#else:
+    #print("4")
+
+
+
+
